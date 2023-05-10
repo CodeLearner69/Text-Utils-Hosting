0becfc574d008896aeed688885977ca673edb7a7
CodeLearner69 <buttt4164@gmail.com>
2023-05-10 21:45:47 +0500

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar(props) {
         <>
             <nav className={`navbar navbar-expand-lg bg-${props.mode}`}>
                 <div className="container-fluid">
-                    <a className={`navbar-brand text-${props.mode === 'dark' ? 'light' : 'dark'}`} href="#">Navbar</a>
+                    <a className={`navbar-brand text-${props.mode === 'dark' ? 'light' : 'dark'}`} href="#">Text Editor</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
